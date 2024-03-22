6b7b22b5e68548990a0f552d862bfa391ae03945
Aika <birnazarova@gmail.com>
2024-03-21 19:24:50 -0500

    added a module for gke

---
+++ gke/examples/main.tf
@@ -0,0 +1,8 @@
+module "gke_cluster" {
+    source = "/Users/aika/Git/tf-modules/gke"
+    service_account_id           = "gke-service-account"
+    service_account_display_name = "GKE Service Account"
+    cluster_name                 = "dev-cluster"
+    cluster_location             = "us-central1-a"
+    project_id                   = "your-project-id"
+}
